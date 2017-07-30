002512e999e525f1346f93fb7a6b06a11fc6be5e
Ehsan Moravveji <u0090231@r6i0n1.icts.leuven.vsc>
2017-07-30 23:00:57 +0200

    rewriting pi/lib.f90 pi_Gregory_Leibniz. two fold speed up on 1 cpu. putting the first OpenACC parallel loop directives

--- pi/lib.f90
+++ pi/lib.f90
@@ -43,12 +43,19 @@ module lib
     real(dp) :: num, denum
 
     sum = 0d0
+    !#acc parallel loop reduction(+:sum) 
     do k = 0, n
-       num   = (-1d0)**k
-       denum = 2d0 * k + 1d0
-       sum   = sum + num / denum
+       sum = sum + fraction(k)
     enddo
+    !#acc end parallel loop
     sum = 4d0 * sum
+
+    contains 
+
+    real(dp) function fraction(k) result(f)
+      integer, intent(in) :: k
+      f = (-1d0)**(mod(k,2)) / (2d0*k + 1d0) 
+    end function fraction
 
   end function pi_Gregory_Leibniz
   
