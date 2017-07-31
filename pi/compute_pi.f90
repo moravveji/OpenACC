c5f40659b89e6338baafa4232160ca86dea9fa1d
Ehsan Moravveji <u0090231@r6i0n1.icts.leuven.vsc>
2017-07-31 21:37:14 +0200

    pi/Makefile updated; now compiling for tesla device; pi_area_rand works

--- pi/compute_pi.f90
+++ pi/compute_pi.f90
@@ -2,15 +2,17 @@
 program compute_pi
   
   use precision
-  use lib, only: pi_Gregory_Leibniz 
+  use lib, only: pi_Gregory_Leibniz, &
+                 pi_area_rand 
 
   implicit none
 
   integer :: none
-  integer, parameter :: N = 1000000
+  integer, parameter :: N = 1000000, M = 1000000
 
   none = check_precision()
 
-  write(*, '(a4, 1x,es22.15e3)') 'pi =', pi_Gregory_Leibniz(N)
+  write(*, '(a, 1x,es22.15e3)') 'pi_gregory_leibniz =', pi_Gregory_Leibniz(N)
+  write(*, '(a, 1x, es22.15e3)') 'pi_area_rand = ', pi_area_rand(M)
 
 end program compute_pi
--- pi/lib.f90
+++ pi/lib.f90
@@ -6,7 +6,8 @@ module lib
   implicit none
 
   private
-  public :: pi_Gregory_Leibniz
+  public :: pi_Gregory_Leibniz, &
+            pi_area_rand
 
   contains
 
@@ -35,12 +36,12 @@ module lib
   end function denumerator
 
   ! =============================================
-  real(dp) function pi_Gregory_Leibniz(n) result(sum)
+  real(dp) function pi_Gregory_Leibniz(n) result(pi)
     integer, intent(in) :: n
     
     integer :: k
     ! double precision :: num, denum
-    real(dp) :: num, denum
+    real(dp) :: sum !num, denum
 
     sum = 0d0
     !#acc parallel loop reduction(+:sum) 
@@ -48,7 +49,7 @@ module lib
        sum = sum + fraction(k)
     enddo
     !#acc end parallel loop
-    sum = 4d0 * sum
+    pi = 4d0 * sum
 
     contains 
 
@@ -60,5 +61,26 @@ module lib
   end function pi_Gregory_Leibniz
   
   ! =============================================
+  real(dp) function pi_area_rand(n) result(pi)
+    integer, intent(in) :: n 
+
+    !integer, parameter :: seed = 12345
+    integer :: i, j, inside
+    real(dp) :: x, y
+    
+    call random_seed()
+
+    inside = 0
+    do i=1, n
+      do j = 1, n
+         call random_number(x)
+         call random_number(y)
+         if (dsqrt(x**2 + y**2) .le. 1d0) inside = inside + 1
+      enddo
+    enddo
+    
+    pi = 4d0 * dble(inside) / dble(n**2)
+
+  end function pi_area_rand
 
 end module lib
