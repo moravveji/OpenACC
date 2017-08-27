97bffb3f0f05e0f9017507c9dd7d483f1fb18502
Ehsan Moravveji <e.moravveji@gmail.com>
2017-08-27 23:38:35 +0200

    book: Makefile improved; compiling for multicore and device; ACC directives added

--- book/fig-1-01.f90
+++ book/fig-1-01.f90
@@ -1,12 +1,20 @@
 
 program fig_1_01
   
+  use openacc
+
   implicit none
   integer :: nCount, nLoop
   integer, dimension(:), allocatable :: array
-  integer :: ierr, k, isum
+  integer :: ierr, j, k, isum
+  logical :: equal
+  real :: start, end
   
-  read*, nCount, nLoop
+  call acc_init(acc_device_nvidia) 
+
+  nCount = 6; nLoop = 40
+  print '(2(a,i4))', 'nCount=', nCount, '; and nLoop=', nLoop 
+!  read *, nCount, nLoop
   if (nCount <= 0 .or. nLoop <= 0) stop 'nCount and nLoop must be greater than zero'
   
   nCount = nCount * 1000000
@@ -14,17 +22,33 @@ program fig_1_01
   allocate(array(nCount), stat=ierr)
   if (ierr /= 0) stop 'Failed to allocate the array(nCount)'
   
-  ! Here is where we fill the status vector
-  do k = 1, nCount
-     array(k) = 1
+  call cpu_time(start)
+
+  !$acc kernels shared(nCount, nLoop) private(isum, array) \
+  !$acc copyin(array)
+  do j = 1, nLoop
+
+    ! Here is where we fill the status vector
+    !$acc loop
+    do k = 1, nCount
+       array(k) = 1
+    enddo
+    !$acc end loop
+
+    isum = 0
+    do k = 1, nCount
+       isum = isum + array(k)
+    enddo 
+
   enddo
+  !$acc end kernels
   
-  isum = 0
-  do k = 1, nCount
-     isum = isum + array(k)
-  enddo 
-  
-  print '(a,i,a)', 'Final sum is ', isum/1000000, 'millions'
-  print '(a,i)', 'assert (sum == nCount)', (isum == nCount)
-  
+  call cpu_time(end)
+
+  equal = .false.
+  equal = (isum == nCount)
+  print '(a,i4,a)', 'Final sum is ', isum/1000000, ' millions'
+  print *, 'assert (sum == nCount):', equal
+  print '(a, f7.4, a)', 'Runtime took ', end-start, ' (sec)'
+
 end program fig_1_01
