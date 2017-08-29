901cb749306d5d24ce8513b4b218e422863ac5f4
Ehsan Moravveji <e.moravveji@gmail.com>
2017-08-30 00:32:54 +0200

    derived_types compiles, and can give back some numbers; work on kern later; saving for now ...

--- derived_types/main.f90
+++ derived_types/main.f90
@@ -6,6 +6,17 @@ program main
   use kern
 
   implicit none
+ 
+  integer :: ierr
+  type(gaussian2d) :: g
+
+  ! Specify the Gaussian curve propertise
+  g% nx = 1000
+  g% ny = 1000
+  g% sx = 1.2345
+  g% sy = 0.8765
+  allocate(g%x(g%nx), g%y(g%ny), g%curve(g%nx, g%ny), stat=ierr)
+  if (ierr /= 0) stop 'Failed to allocate derived type allocatable arrays'
 
   ! copy the derived type from host to device
   call h2d(g)
@@ -15,5 +26,7 @@ program main
 
   ! copy the useful results back to the host
   call d2h(g)
+
+  print*, g%curve(1,1)
 
 end program main 
--- derived_types/vars.f90
+++ derived_types/vars.f90
@@ -6,7 +6,6 @@ module vars
   public
 
   type gaussian2d
-    integer :: ratio   ! sy / sx
     real :: sx, sy     ! dispersions
     integer :: nx, ny  ! number of points along each direction
     real, dimension(:), allocatable :: x, y ! coordinates
@@ -20,7 +19,7 @@ module vars
   subroutine h2d(a)
     type(gaussian2d) :: a
     !$acc enter data copyin(a)
-    !$acc enter data create(a%ratio, a%sx, a%sy, a%nx, a%ny, a%x, a%y, a%curve)
+    !$acc enter data create(a%sx, a%sy, a%nx, a%ny, a%x, a%y, a%curve)
   end subroutine h2d
 
   subroutine d2h(b)
