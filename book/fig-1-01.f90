0a40e300e62edfa023d79e2427a8bf755ec000a9
Ehsan Moravveji <e.moravveji@gmail.com>
2017-08-29 13:47:23 +0200

    Makefile supporting to compile both CPU and GPU versions

--- book/fig-1-01.f90
+++ book/fig-1-01.f90
@@ -1,8 +1,6 @@
 
 program fig_1_01
   
-!  use openacc
-
   implicit none
   integer :: nCount
   integer, dimension(:), allocatable :: array
@@ -10,9 +8,7 @@ program fig_1_01
   logical :: equal
   real :: start, end
   
-!  call acc_init(acc_device_nvidia) 
-
-  nCount = 1000
+  nCount = 100
   print '(a,i4)', 'nCount=', nCount
 
   if (nCount <= 0) stop 'nCount must be greater than zero'
@@ -25,24 +21,26 @@ program fig_1_01
 
   call cpu_time(start)
 
+  isum = 0
+ 
   ! Here is where we fill the status vector
-  !$acc parallel loop copyout(array(1:nCount)) 
+  !$acc parallel loop copyin(array(1:nCount)) copyout(isum)
   do k = 1, nCount
      array(k) = 1
   enddo
-  !$acc end parallel loop
   
-  isum = 0
+  !$acc loop reduction(+:isum)
   do k = 1, nCount
      isum = isum + array(k)
   enddo 
+  !$acc end loop
 
   call cpu_time(end)
 
   equal = .false.
   equal = (isum == nCount)
   print '(a,i4,a)', 'Final sum is ', isum/1000000, ' millions'
-  print *, 'assert (sum == nCount):', equal
+  print *, 'assert (isum == nCount):', equal
   print '(a, f7.4, a)', 'Runtime took ', end-start, ' (sec)'
 
 end program fig_1_01
